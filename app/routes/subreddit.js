9a3817b97e4bd4b9d55f9d4b26fa9305a917ff93
ankeetmaini <ankeet.maini@gmail.com>
2014-10-23 11:15:26 +0530

    fixed model hook of subreddit by passing id

--- app/routes/subreddit.js
+++ app/routes/subreddit.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	setupController: function(controller, model){
-		var adapter = this.container.lookup("adapter:reddit");
-		controller.set("model", adapter.getReddits(model.id));
-	},
 	model: function(params) {
 		var adapter = this.container.lookup("adapter:reddit");
 		return adapter.getReddits(params.subreddit_id);
